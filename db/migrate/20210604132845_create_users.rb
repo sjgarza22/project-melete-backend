14678e4414d4921c8a6daaf76a8393af0b31c091
sjgarza22 <sjgarza22@gmail.com>
2021-06-04 14:28:37 -0500

    Added username to Users table

--- db/migrate/20210604132845_create_users.rb
+++ db/migrate/20210604132845_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :email
+      t.string :username
       t.string :password_digest
       t.string :user_type
       t.string :name
